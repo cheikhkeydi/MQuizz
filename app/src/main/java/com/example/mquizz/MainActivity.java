af90ee23ec9d8c733edb1e336f175e0e8f75e224
Cheikh <cheikhkeydi95@gmail.com>
2021-03-30 10:36:14 -0700

    Repartition des taches

--- app/src/main/java/com/example/mquizz/MainActivity.java
+++ app/src/main/java/com/example/mquizz/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.mquizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button scripte,acceuil,niveau,info,jouer,filiere,matiere,score,questionnaire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scripte=(Button) findViewById(R.id.scripte);
+        acceuil=(Button) findViewById(R.id.acceuil);
+        niveau=(Button) findViewById(R.id.niveau);
+        info=(Button) findViewById(R.id.jouer);
+        filiere=(Button) findViewById(R.id.filiere);
+        matiere=(Button) findViewById(R.id.matiere);
+        score=(Button) findViewById(R.id.score);
+        questionnaire=(Button) findViewById(R.id.questionnaire);
+        jouer=(Button) findViewById(R.id.jouer);
+
+        //Redirection des boutons vers les pages correspondantes
+        scripte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent helloworld1= new Intent(getApplicationContext(), scripte1.class);
+                startActivity(helloworld1);
+            }
+        });
+        //QUestionnaire1
+        questionnaire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent helloworld1= new Intent(getApplicationContext(), q1.class);
+                startActivity(helloworld1);
+            }
+        });
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/mquizz/q1.java
@@ -0,0 +1,14 @@
+package com.example.mquizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class q1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q1);
+    }
+}
